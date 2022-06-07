deb44794308716e25d4c453f5af71320a992d360
Hardy <hardy40105@gmail.com>
2022-06-08 00:25:26 +0800

    backup 2022-06-08 00:25

--- app/src/main/java/com/msi/stockmanager/ui/main/pager/cash/CashFragment.java
+++ app/src/main/java/com/msi/stockmanager/ui/main/pager/cash/CashFragment.java
@@ -3,6 +3,7 @@ package com.msi.stockmanager.ui.main.pager.cash;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.widget.PopupMenu;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
@@ -21,6 +22,7 @@ import com.msi.stockmanager.R;
 import com.msi.stockmanager.data.DateUtil;
 import com.msi.stockmanager.data.transaction.ITransApi;
 import com.msi.stockmanager.data.transaction.TransApi;
+import com.msi.stockmanager.data.transaction.TransType;
 import com.msi.stockmanager.data.transaction.Transaction;
 import com.msi.stockmanager.database.DBDefine;
 
@@ -35,8 +37,8 @@ public class CashFragment extends Fragment {
     private static final String TAG = CashFragment.class.getSimpleName();
     private TextView CashIn,CashOut;
     private ListView listview;
-    private double cashin_int=0,cashout_int=0;
-    private ArrayList<HashMap> listAdapter;
+    private int cashin_int=0,cashout_int=0;
+    private ArrayList<Transaction> listAdapter = new ArrayList<>();;
     private CashAdapter adapter;
     ITransApi transApi = null;
 
@@ -81,94 +83,78 @@ public class CashFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         transApi = new TransApi(getContext());
         initView();
-        registerForContextMenu(listview);
+//        registerForContextMenu(listview);
     }
 
     void initView(){
         CashIn = getView().findViewById(R.id.CashIn);
         CashOut = getView().findViewById(R.id.CashOut);
         listview = getView().findViewById(R.id.listView1);
-        adapter = new CashAdapter(getContext());
-        listview.setAdapter(adapter);
-        listview.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+        adapter = new CashAdapter(getContext(), new CashAdapter.ItemLongClickListener() {
             @Override
-            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
-                return false;
+            public void onLongClick(View view, int position, Transaction trans) {
+                PopupMenu popupMenu = new PopupMenu(getContext(), view);
+
+                // Inflating popup menu from popup_menu.xml file
+                popupMenu.getMenuInflater().inflate(R.menu.item_edit, popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+                        switch(item.getItemId())
+                        {
+                            case R.id.item1:
+                                Log.d("ContextItem","Edit"+info.position);
+                                Intent intent = new Intent(getContext(), InputCashInOut.class);
+                                intent.putExtra("transId", listAdapter.get(info.position).trans_id);
+                                startActivity(intent);
+
+
+                                return true;
+                            case R.id.item2:
+                                Log.d("ContextItem","Del"+info.position);
+                                transApi.removeTrans(listAdapter.get(info.position).trans_id);
+                                showInList();
+
+                                return true;
+
+                        }
+                        return onOptionsItemSelected(item);
+                    }
+                });
+                // Showing the popup menu
+                popupMenu.show();
             }
         });
+        listview.setAdapter(adapter);
     }
 
 
     private void showInList(){
-        listAdapter = new ArrayList<HashMap>();
-        List records = transApi.getHistoryTransList();
+        listAdapter.clear();
+        List<Transaction> records = transApi.getHistoryTransList();
         cashin_int=0;
         cashout_int=0;
 
-        for (int i=0;i<records.size();i++) {
-            Transaction item_tran = (Transaction) records.get(i);
-
+        for (Transaction item_tran: records) {
+            if(item_tran.trans_type != TransType.TRANS_TYPE_CASH_IN &&
+                item_tran.trans_type != TransType.TRANS_TYPE_CASH_OUT){
+                continue;
+            }
             if (item_tran.stock_id.equals("") && item_tran.cash_amount != 0) {
-                HashMap item = new HashMap();
-                item.put(DBDefine.TB_TransactionRecord.COLUMN_NAME_TRANSACTION_TYPE, item_tran.trans_type);
-                item.put(DBDefine.TB_TransactionRecord.COLUMN_NAME_CASH_AMOUNT, item_tran.cash_amount);
-                item.put(DBDefine.TB_TransactionRecord.COLUMN_NAME_TRANSACTION_TIME, DateUtil.toDateString(item_tran.trans_time));
-                item.put("trans_id",item_tran.trans_id);
-                Log.d("showInList", "trans_id="+item_tran.trans_id+" "+item_tran.stock_id+" "+item_tran.cash_amount+" "+item_tran.trans_type+" "+DateUtil.toDateString(item_tran.trans_time));
-                listAdapter.add(item);
-
-                if (item_tran.trans_type == 2)
-                    cashin_int = cashin_int + item_tran.cash_amount;
+                listAdapter.add(item_tran);
+                if (item_tran.trans_type == TransType.TRANS_TYPE_CASH_IN)
+                    cashin_int += item_tran.cash_amount;
                 else
-                    cashout_int = cashout_int + item_tran.cash_amount;
+                    cashout_int += item_tran.cash_amount;
             }
         }
         CashIn.setText(cashin_int+"");
-        CashOut.setText(cashout_int+"");
+        CashOut.setText(-cashout_int+"");
 
         adapter.setItems(listAdapter);
         adapter.notifyDataSetChanged();
 
-    }
-
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo)
-    {
-        super.onCreateContextMenu(menu, v, menuInfo);
-
-        MenuInflater inflater = getActivity().getMenuInflater();
-        inflater.inflate(R.menu.item_edit, menu);
-    }
-
-    private int transId = 0;
-    @Override
-    public boolean onContextItemSelected(MenuItem item)
-    {
-        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
-        switch(item.getItemId())
-        {
-            case R.id.item1:
-                Log.d("ContextItem","Edit"+info.position);
-                transId = Integer.parseInt(listAdapter.get(info.position).get("trans_id").toString());
-                Log.d("ContextItem",transId+"");
-                Intent intent = new Intent(getContext(), InputCashInOut.class);
-                intent.putExtra("transId", transId);
-                startActivity(intent);
-
-
-                return true;
-            case R.id.item2:
-                Log.d("ContextItem","Del"+info.position);
-
-                transId = Integer.parseInt(listAdapter.get(info.position).get("trans_id").toString());
-
-                transApi.removeTrans(transId);
-                showInList();
-
-                return true;
-
-        }
-        return onOptionsItemSelected(item);
     }
 
     @Override
