9e0985199a25b1c4d7336c4476336102b360eef4
hardycheng <hardy40105@gmail.com>
2022-06-14 17:37:30 +0800

    backup 2022-06-14 17:37

--- app/src/main/java/com/msi/stockmanager/data/stock/IStockApi.java
+++ app/src/main/java/com/msi/stockmanager/data/stock/IStockApi.java
@@ -1,6 +1,7 @@
 package com.msi.stockmanager.data.stock;
 
 public interface IStockApi {
+    int LAST_UPDATE_INTERVAL = 3600000; //1hr
 
     void getRegularStockPrice(String stock_id, ResultCallback callback);
 
